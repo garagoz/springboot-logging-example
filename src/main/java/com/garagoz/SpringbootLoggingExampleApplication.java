09056f315f0517171ce77bfb8afa64a49e8234b2
arslan <atorayew@gmail.com>
2023-12-25 10:38:35 +0500

    First project create commit

---
+++ src/main/java/com/garagoz/SpringbootLoggingExampleApplication.java
@@ -0,0 +1,13 @@
+package com.garagoz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLoggingExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLoggingExampleApplication.class, args);
+	}
+
+}
